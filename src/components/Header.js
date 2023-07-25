3231de2b0d318224e41ba900909b1a4d50b99c35
adnanali17official <adnanali17official@gmail.com>
2023-07-25 22:41:27 +0500

    initial push

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Octicon from "react-octicon";
+import Search from "./Search";
+import { getPublicGists } from "../services/gistService";
+
+function Header() {
+  useEffect(() => {
+    getPublicGists();
+  }, []);
+
+  return (
+    <Wrapper>
+      <Octicon name="mark-github" mega />
+      <Search />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  background-color: #24292e;
+  color: #ffffff;
+  z-index: 32;
+  padding: 16px;
+  font-size: 14px;
+  line-height: 1.5;
+  display: flex;
+  align-items: center;
+`;
+
+export default Header;
